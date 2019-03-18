dc7d17c59f1f55ee5908a12a867db6f9fd8a1df9
reydon227 <dhruvarora@delhinerds.com>
2019-03-18 19:53:01 +0530

    loign and login otp screens

--- App/Containers/Login/LoginOTPScreen/LoginOTPScreen.js
+++ App/Containers/Login/LoginOTPScreen/LoginOTPScreen.js
@@ -1,21 +1,93 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, {Component} from 'react'
+import { Text, View, TextInput, StyleSheet, TouchableHighlight } from 'react-native'
 import NavigationService from 'App/Services/NavigationService'
-import { Button } from '@shoutem/ui'
+import { Button, Screen, ImageBackground } from '@shoutem/ui'
 
-export default class SplashScreen extends React.Component {
-  goToOTPScreen() {
-    NavigationService.navigate('LoginOTPScreen')
+const styles = StyleSheet.create({
+  bgImage:{
+    backgroundColor: "black",
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    opacity: 0.8
+  },
+  phoneNumber:{
+    width: "70%",
+    opacity: 1,
+    color: "white",
+    height: 25,
+    marginBottom: 4,
+    fontSize: 15,
+    borderBottomWidth: 3,
+    borderBottomColor: "green",
+    textAlign: "center"
+  },
+  phoneNumber2:{
+    width: "70%",
+    opacity: 1,
+    color: "white",
+    height: 25,
+    marginBottom: 4,
+    fontSize: 15,
+    borderBottomWidth: 3,
+    borderBottomColor: "white",
+    textAlign: "center"
+  },
+  button:{
+    padding: 10,
+    backgroundColor: "white"    
+  },
+  msgBox:{
+    padding: 10,
+    backgroundColor: "green",
+    marginBottom : 10,
+    justifyContent : "center",
+    textAlign : "center"
+  },
+  center: {
+    color: "white",
+    textAlign: "center"
+  }
+})
+
+export default class LoginOTPScreen extends Component {
+
+  constructor(props){
+    super(props);
+    this.mobile = this.props.navigation.getParam("mobile", "NA")
+    this.state = {
+      isFocused : false,
+      otp : ''
+    }
   }
 
   render() {
+
     return (
-      <View>
-        <Text>Login OTP Screen</Text>
-        <Button styleName="secondary">
-          <Text>Verify OTP</Text>
-        </Button>
-      </View>
+      <Screen>
+        <ImageBackground
+          style={styles.bgImage}
+          source={require("../../../Images/logo.png")}
+        >
+        <View style={styles.msgBox}>
+          <TouchableHighlight onPress={ ()=> {this.props.navigation.goBack()} }>
+            <View>
+              <Text style={styles.center}>The mobile number you entered is {this.mobile}</Text>
+              <Text style={styles.center}>Click on this box to edit number</Text>
+            </View>            
+          </TouchableHighlight>
+        </View>
+        <TextInput 
+          placeholder={"Enter OTP"}
+          style={styles.phoneNumber2} 
+          placeholderTextColor="white"           
+          onChangeText = { (text)=> {this.setState({otp:text})}}
+        />
+        <Button styleName="secondary" style={styles.button}>
+          <Text>Press to continue</Text>
+        </Button>        
+        </ImageBackground>
+      </Screen>  
     )
   }
 }
--- App/Containers/Login/LoginScreen/LoginScreen.js
+++ App/Containers/Login/LoginScreen/LoginScreen.js
@@ -1,21 +1,79 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, {Component} from 'react'
+import { Text, View, StyleSheet, Image, TextInput } from 'react-native'
 import NavigationService from 'App/Services/NavigationService'
-import { Button } from '@shoutem/ui'
+import { Button, ImageBackground, Screen } from '@shoutem/ui'
 
-export default class SplashScreen extends React.Component {
+const styles = StyleSheet.create({
+  bgImage:{
+    backgroundColor: "black",
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    opacity: 0.8
+  },
+  phoneNumber:{
+    width: "70%",
+    opacity: 1,
+    color: "white",
+    height: 25,
+    marginBottom: 10,
+    fontSize: 15,
+    borderBottomWidth: 3,
+    borderBottomColor: "green",
+    textAlign: "center"
+  },
+  phoneNumber2:{
+    width: "70%",
+    opacity: 1,
+    color: "white",
+    height: 25,
+    marginBottom: 4,
+    fontSize: 15,
+    borderBottomWidth: 3,
+    borderBottomColor: "white",
+    textAlign: "center"
+  },
+  button:{
+    padding: 10,
+    backgroundColor: "white"    
+  }
+})
+
+export default class LoginScreen extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      mobile : ''
+    }
+  }
+
   goToOTPScreen() {
-    NavigationService.navigate('LoginOTPScreen')
+    NavigationService.navigate('LoginOTPScreen', {
+      mobile: this.state.mobile
+    })
   }
 
   render() {
     return (
-      <View>
-        <Text>Login Screen</Text>
-        <Button styleName="secondary" onPress={() => this.goToOTPScreen()}>
-          <Text>CHECK IN HERE</Text>
+      <Screen>
+        <ImageBackground  
+          style={styles.bgImage}
+          source={require("../../../Images/bg.jpg")}
+        >
+        <TextInput 
+          placeholder={"Enter Phone Number"}
+          style={styles.phoneNumber}
+          placeholderTextColor="white"
+          onChangeText = {(text)=>{this.setState({mobile:text})}}
+        />
+        <Button styleName="secondary" style={styles.button} onPress={
+          () => this.goToOTPScreen()          
+        }>
+          <Text>Generate OTP</Text>
         </Button>
-      </View>
+        </ImageBackground>
+      </Screen>                
     )
   }
 }
