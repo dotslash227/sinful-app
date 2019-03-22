9ab98c096f9c71e0b7eb16b7a46aed8b20671cf3
reydon227 <dhruvarora@delhinerds.com>
2019-03-22 21:49:56 +0530

    work on address chooser

--- App/Containers/Login/AddressChooser/AddressChooser.js
+++ App/Containers/Login/AddressChooser/AddressChooser.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react'
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
 import NavigationService from 'App/Services/NavigationService'
-import {Container, Grid, Row, Col, Icon, Form, Input, Item, Label, Button} from 'native-base'
+import {Container, Grid, Row, Col, Icon, Form, Input, Item, Label, Button, FooterTab, Footer} from 'native-base'
 import RNGooglePlaces from 'react-native-google-places';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 
 export class LocationBar extends Component{
     constructor(props){
@@ -45,7 +46,9 @@ export default class AddressChooser extends Component{
             locations : [],
             showLocations: false,
             pickedLocation: '',
-            loading: true
+            loading: true,            
+            latlng: '',
+            flag: false
         };        
     }
 
@@ -54,16 +57,24 @@ export default class AddressChooser extends Component{
         .then((result)=>{
             this.setState({
                 userLat: result[0].location.latitude,
-                userLong: result[0].location.longitude
+                userLong: result[0].location.longitude,
+                latlng : {latitude: result[0].location.latitude, longitude:result[0].location.longitude}
             })
             this.setState({loading:false})
         })
         .catch((error)=>console.log(error.message))
     }
 
-    locationPickerHandler(location){
-        console.log(location);
-        this.setState({pickedLocation:location, showLocations:false})
+    locationPickerHandler(location){        
+        RNGooglePlaces.lookUpPlaceByID(location.placeID)
+        .then((results)=>{
+            this.setState({
+                pickedLocation:{latitude:results.location.latitude, longitude:results.location.longitude}, 
+                latlng : {latitude:results.location.latitude, longitude:results.location.longitude}, 
+                showLocations:false
+            })
+        })
+        .catch((error)=>console.log(error.message));        
     }
 
     googlePlacesFunction(text){
@@ -123,13 +134,34 @@ export default class AddressChooser extends Component{
                         </View>                
                     </View>                
     
-                    <View style={styles.container}>
-                        <Text style={styles.containerText}>For Google Maps</Text>
-                    </View>
-    
-                    <Button full disabled style={styles.nextButton}>
-                        <Text>Next</Text>
-                    </Button>
+                    <MapView
+                        provider = {PROVIDER_GOOGLE}
+                        style={styles.map}
+                        region={{
+                        latitude: (this.state.pickedLocation=='')?this.state.userLat:this.state.pickedLocation.latitude,
+                        longitude: (this.state.pickedLocation=='')?this.state.userLong:this.state.pickedLocation.longitude,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421
+                        }}
+                        showsUserLocation={true}
+                        minZoomLevel={15}
+                        maxZoomLevel={20}
+                    >
+                    
+                    <Marker
+                        coordinate = {this.state.latlng}
+                        draggable                
+                    />
+                    
+                    </MapView>
+
+                    <Footer style={{position:"absolute", top:500}}>
+                        <FooterTab>
+                            <Button full disabled style={styles.nextButton}>
+                                <Text>Next</Text>
+                            </Button>
+                        </FooterTab>
+                    </Footer>                    
                 </Container>
             )
         }        
@@ -138,6 +170,13 @@ export default class AddressChooser extends Component{
 }
 
 const styles = StyleSheet.create({
+    map:{
+        position: "absolute",
+        top: 175,
+        height: 350,
+        width: 450,
+        zIndex: -1
+    },  
     secondaryText:{
         fontSize: 10
     },
--- android/app/src/main/java/com/sinful/MainApplication.java
+++ android/app/src/main/java/com/sinful/MainApplication.java
@@ -3,6 +3,7 @@ package com.sinful;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
 import com.reactnative.photoview.PhotoViewPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
             new RNGooglePlacesPackage(),
             new PhotoViewPackage(),
             new LinearGradientPackage(),
