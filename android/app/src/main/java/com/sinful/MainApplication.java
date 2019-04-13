a6d3f909b598c19a3280bb71023e8f93c1f52948
Abhishek Biswal <wirddin@gmail.com>
2019-04-13 16:02:20 +0530

    Fixed android build issue

--- android/app/src/main/java/com/sinful/MainApplication.java
+++ android/app/src/main/java/com/sinful/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.RNFirebasePackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
 import com.reactnative.photoview.PhotoViewPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new RNFirebasePackage(),
             new MapsPackage(),
             new RNGooglePlacesPackage(),
