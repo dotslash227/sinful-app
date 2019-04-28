2507cdb3688617d6853b6ffcee6f82c81eafac3f
Abhishek Biswal <wirddin@gmail.com>
2019-04-28 22:54:24 +0530

    Print user info on settings tab

--- App/Containers/Home/Settings/Settings.js
+++ App/Containers/Home/Settings/Settings.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Platform, Text, View, Button } from 'react-native';
-import { Container, Content, Icon } from 'native-base';
+import { connect } from 'react-redux';
+import { Container, Content, Icon, H1, H3 } from 'native-base';
+
+// Components
 import HeaderComponent from 'App/Components/Header';
 
 class SettingsScreen extends React.Component {
@@ -10,13 +13,24 @@ class SettingsScreen extends React.Component {
 	}
 
 	render() {
+		const { user } = this.props;
+		const { profile } = user;
+
 		return (
 			<Container>
 				<HeaderComponent title="Settings" />
-				<Content />
+				<Content>
+					<H1>{profile.name}</H1>
+					<H3>{profile.email}</H3>
+				</Content>
 			</Container>
 		);
 	}
 }
 
-export default SettingsScreen;
+const mapStateToProps = (state) => {
+	const { user } = state;
+	return { user };
+};
+
+export default connect(mapStateToProps)(SettingsScreen);
