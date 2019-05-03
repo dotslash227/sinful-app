4a6e788c198b131c2a147a07ded09ea28318e033
Abhishek Biswal <wirddin@gmail.com>
2019-05-03 12:00:16 +0530

    Added Restaurants to Redux state

--- App/Containers/Home/Restaurants/Restaurants.js
+++ App/Containers/Home/Restaurants/Restaurants.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Platform, Text, View, Button } from 'react-native';
+import { connect } from 'react-redux';
 import { Container, Content, Icon } from 'native-base';
 
 // Components
 import HeaderComponent from 'App/Components/Header';
 import RestaurantCard from 'App/Components/Restaurants/RestaurantCard';
 
-import restaurants from 'App/Data/fake-restaurants.json';
-
-class HomeScreen extends React.Component {
+class RestaurantsScreen extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {};
 	}
 
 	render() {
-		const list = restaurants.map((r) => {
+		const { items } = this.props.restaurants;
+		const list = items.map((r) => {
 			return <RestaurantCard key={r.id} restaurant={r} />;
 		});
 		return (
@@ -29,4 +29,9 @@ class HomeScreen extends React.Component {
 	}
 }
 
-export default HomeScreen;
+const mapStateToProps = (state) => {
+	const { user, restaurants } = state;
+	return { user, restaurants };
+};
+
+export default connect(mapStateToProps)(RestaurantsScreen);
